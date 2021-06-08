0c6a4d867830f79410e5cc535ff3c2908da9b226
gaye <gayegayemboup@gmail.com>
2021-06-08 11:50:38 +0200

    creation d'une société

--- src/Controller/company/newCompanyController.php
+++ src/Controller/company/newCompanyController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Controller\company;
-
+use App\Entity\Company;
+use App\Form\NewCompanyType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class newCompanyController extends AbstractController
@@ -11,10 +14,28 @@ class newCompanyController extends AbstractController
     /**
      * @Route("/company/new/company", name="company_new_company")
      */
-    public function index(): Response
+    // public function index(): Response
+    // {
+    //     return $this->render('company/new_company/index.html.twig', [
+    //         'controller_name' => 'newCompanyController',
+    //     ]);
+    // }
+    public function newCompany(Request $request): Response
     {
+        $company = new Company();
+        $form = $this->createForm(NewCompanyType::class, $company);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($company);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+
+        }
         return $this->render('company/new_company/index.html.twig', [
-            'controller_name' => 'newCompanyController',
+            "form" => $form->createView()
         ]);
     }
 }
