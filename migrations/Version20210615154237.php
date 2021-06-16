af243a9b47314c18d902678c6d6b998158f01326
heinzcodeur <heinzcodeur@gmail.com>
2021-06-16 12:23:47 +0200

    type

--- migrations/Version20210615154237.php
+++ migrations/Version20210615154237.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210615120802 extends AbstractMigration
+final class Version20210615154237 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,8 @@ final class Version20210615120802 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE company ADD forme_legale_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE forme_legale (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, shortname VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE company ADD city VARCHAR(50) NOT NULL, DROP form_legal');
         $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094FE48191B FOREIGN KEY (forme_legale_id) REFERENCES forme_legale (id)');
         $this->addSql('CREATE INDEX IDX_4FBF094FE48191B ON company (forme_legale_id)');
     }
@@ -29,7 +30,8 @@ final class Version20210615120802 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE company DROP FOREIGN KEY FK_4FBF094FE48191B');
+        $this->addSql('DROP TABLE forme_legale');
         $this->addSql('DROP INDEX IDX_4FBF094FE48191B ON company');
-        $this->addSql('ALTER TABLE company DROP forme_legale_id');
+        $this->addSql('ALTER TABLE company ADD form_legal VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP city');
     }
 }
